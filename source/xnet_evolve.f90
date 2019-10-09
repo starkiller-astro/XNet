581e66349b9e4d0dce477078ccff7f8346836b8d
Austin Harris <jaharris87@users.noreply.github.com>
2019-10-09 10:51:39 -0400

    OpenACC for main xnet evolution routine

--- source/xnet_evolve.f90
+++ source/xnet_evolve.f90
@@ -14,18 +14,18 @@ Module xnet_evolve
 
 Contains
 
-  Subroutine full_net
+  Subroutine full_net(kstep)
     !-----------------------------------------------------------------------------------------------
     ! The abundance evolution is performed over a series of timesteps, with the duration of the
     ! timestep determined by the integration scheme and the changing thermodynamic conditions.
     ! Integration is performed by a choice of methods controlled by the isolv flag.
     !-----------------------------------------------------------------------------------------------
-    Use nuclear_data, Only: ny, nname, benuc
+    Use nuclear_data, Only: ny, nname
     Use xnet_abundances, Only: yo, y, yt, ydot
-    Use xnet_conditions, Only: t, to, tt, tdel, tdel_old, tdel_next, t9, t9o, t9t, t9dot, rho, rhoo, &
-      & rhot, yeo, ye, yet, nt, nto, ntt, tstart, tstop, t9rhofind
+    Use xnet_conditions, Only: t, to, tt, tdel, tdel_old, tdel_next, t9, t9o, t9t, t9dot, &
+      & rho, rhoo, rhot, yeo, ye, yet, nt, nto, ntt, tstop, t9rhofind
     Use xnet_controls, Only: idiag, iheat, isolv, itsout, kstmx, kmon, ktot, lun_diag, lun_stdout, &
-      & lzactive, szbatch, nzbatchmx, nzevolve, zb_lo, zb_hi
+      & lzactive, szbatch, nzbatchmx, nzevolve, zb_lo, zb_hi, tid
     Use xnet_integrate, Only: timestep
     Use xnet_integrate_be, Only: solve_be
     Use xnet_integrate_bdf, Only: solve_bdf
@@ -35,30 +35,26 @@ Contains
     Use xnet_util, Only: xnet_terminate
     Implicit None
 
+    ! Output variables
+    Integer, Intent(out) :: kstep
+
     ! Local variables
     !Integer, Parameter :: kstep_output = 10
-    Real(dp) :: enm(zb_lo:zb_hi), enb(zb_lo:zb_hi)
-    Real(dp) :: enold(zb_lo:zb_hi), en0(zb_lo:zb_hi)
-    Real(dp) :: delta_en(zb_lo:zb_hi), edot(zb_lo:zb_hi)
-    Integer :: k, izb, izone, kstep, nstep_est, idiag0
+    Real(dp) :: enm, enb, enold(zb_lo:zb_hi), en0(zb_lo:zb_hi)
+    Integer :: k, izb, izone, nstep_est, idiag0
     Integer :: its(zb_lo:zb_hi), mykstep(zb_lo:zb_hi)
     Logical :: lzsolve(zb_lo:zb_hi), lzoutput(zb_lo:zb_hi)
 
     start_timer = xnet_wtime()
     timer_xnet = timer_xnet - start_timer
 
-    ! Initialize counters
-    kstep = 0
-    Do izb = zb_lo, zb_hi
-      kmon(:,izb) = 0
-      ktot(:,izb) = 0
-    EndDo
-
     ! Set reaction controls not read in from control
     idiag0 = idiag
 
     ! Initialize trial time step abundances and conditions
-    Call t9rhofind(0,t,nt,t9,rho)
+    !$acc parallel loop gang async(tid) &
+    !$acc present(kmon,ktot,tdel,tdel_old,tdel_next,nt,nto,ntt, &
+    !$acc         t9,t9o,t9t,rho,rhoo,rhot,t,to,tt,ye,yeo,yet,y,yo,yt)
     Do izb = zb_lo, zb_hi
       tdel_old(izb) = tdel(izb)
       tdel_next(izb) = tdel(izb)
@@ -70,43 +66,39 @@ Contains
       t9t(izb) = t9(izb)
       rhoo(izb) = rho(izb)
       rhot(izb) = rho(izb)
-      yet(izb) = ye(izb)
       yeo(izb) = ye(izb)
-      yo(:,izb) = y(:,izb)
-      yt(:,izb) = y(:,izb)
-    EndDo
-
-    en0 = 0.0
-    enm = 0.0
-    delta_en = 0.0
-    edot = 0.0
-    Do izb = zb_lo, zb_hi
-      If ( lzactive(izb) ) Then
-
-        ! Calculate the total energy of the nuclei
-        Call benuc(yt(:,izb),enb(izb),enm(izb))
-        en0(izb) = enm(izb)
-        delta_en(izb) = 0.0
-        edot(izb) = 0.0
-
-        If ( itsout > 0 ) Write(lun_stdout,"(a,i6,a,i2,2(a,es10.3))") &
-          & 'Max Step',kstmx,' IDiag=',idiag,' Start Time',tstart(izb),' Stop Time',tstop(izb)
-      EndIf
+      yet(izb) = ye(izb)
+      !$acc loop vector
+      Do k = 1, ny
+        yo(k,izb) = y(k,izb)
+        yt(k,izb) = y(k,izb)
+      EndDo
+      !$acc loop vector
+      Do k = 1, 5
+        kmon(k,izb) = 0
+        ktot(k,izb) = 0
+      EndDo
     EndDo
 
     ! Output initial abundances and conditions
-    Call ts_output(0,delta_en,edot)
+    Call ts_output(0,en0,enold)
 
-    ! Start evolution
+    ! Initialize timestep loop flags
+    kstep = 0
+    mykstep = 0
+    lzsolve = lzactive(zb_lo:zb_hi)
+    lzoutput = lzsolve
     Do izb = zb_lo, zb_hi
-      If ( lzactive(izb) ) Then
+      If ( lzsolve(izb) ) Then
         its(izb) = 0
       Else
         its(izb) = -1
       EndIf
-      lzsolve(izb) = lzactive(izb)
-      mykstep(izb) = 0
     EndDo
+
+    ! Start evolution
+    !$acc enter data async(tid) &
+    !$acc copyin(its,mykstep,lzsolve,lzoutput)
     Do kstep = 1, kstmx
 
       ! Determine if this is an output step
@@ -126,49 +118,71 @@ Contains
         Call solve_be(kstep,its)
       End Select
 
-      Do izb = zb_lo, zb_hi
-        izone = izb + szbatch - zb_lo
-
-        ! If convergence is successful, output timestep results
-        If ( its(izb) == 0 ) Then
-          If ( idiag >= 1 ) Write(lun_diag,"(2(a,i5),5es14.7)") &
-            & 'KStep ',kstep,' Zone ',izone,t(izb),tdel(izb),t9(izb),rho(izb),ye(izb)
-          If ( idiag >= 3 ) Then
-            Write(lun_diag,"(a8)") 'delta Y'
-            Write(lun_diag,"(8x,a5,4es12.4)") &
-              & (nname(k),y(k,izb),yo(k,izb),(y(k,izb)-yo(k,izb)),(tdel(izb)*ydot(k,izb)),k=1,ny)
-            If ( iheat > 0 ) Write(lun_diag,"(8x,a5,4es12.4)") &
-              & 'T9',t9(izb),t9o(izb),t9(izb)-t9o(izb),tdel(izb)*t9dot(izb)
+      ! If convergence is successful, output timestep results
+      If ( idiag >= 1 ) Then
+        !$acc update wait(tid) &
+        !$acc host(its)
+        Do izb = zb_lo, zb_hi
+          izone = izb + szbatch - zb_lo
+          If ( its(izb) == 0 .and. idiag >= 1 ) Then
+            !$acc update wait(tid) &
+            !$acc host(t(izb),tdel(izb),t9(izb),rho(izb),ye(izb))
+            Write(lun_diag,"(2(a,i5),5es14.7)") &
+              & 'KStep ',kstep,' Zone ',izone,t(izb),tdel(izb),t9(izb),rho(izb),ye(izb)
+            If ( idiag >= 3 ) Then
+              !$acc update wait(tid) &
+              !$acc host(y(:,izb),yo(:,izb),ydot(:,izb),tdel(izb),&
+              !$acc      t9(izb),t9o(izb),t9dot(izb))
+              Write(lun_diag,"(a8)") 'delta Y'
+              Write(lun_diag,"(8x,a5,4es12.4)") &
+                & (nname(k),y(k,izb),yo(k,izb),(y(k,izb)-yo(k,izb)),(tdel(izb)*ydot(k,izb)),k=1,ny)
+              If ( iheat > 0 ) Write(lun_diag,"(8x,a5,4es12.4)") &
+                & 'T9',t9(izb),t9o(izb),t9(izb)-t9o(izb),tdel(izb)*t9dot(izb)
+            EndIf
+          ElseIf ( its(izb) == 1 ) Then
+            Write(lun_diag,"(a,2(i5,a))") 'KStep ',kstep,' Zone ',izone,' Inter!!!'
           EndIf
-          enold(izb) = enm(izb)
-          Call benuc(yt(:,izb),enb(izb),enm(izb))
-          delta_en(izb) = enm(izb) - en0(izb)
-          edot(izb) = -(enm(izb)-enold(izb)) / tdel(izb)
+        EndDo
+        Flush(lun_diag)
+      EndIf
+
+      !$acc parallel loop gang async(tid) &
+      !$acc present(its,t,tstop,mykstep,lzsolve,lzoutput)
+      Do izb = zb_lo, zb_hi
+        If ( its(izb) == 0 ) Then
 
           ! If this zone reaches the stop time, flag it to remove from loop
           If ( t(izb) >= tstop(izb) ) Then
             mykstep(izb) = kstep
             its(izb) = -1
+            lzsolve(izb) = .false.
           EndIf
 
-        ! If reduced timesteps fail to successfully integrate, warn and flag to remove from loop
+        ! If reduced timesteps fail to successfully integrate, flag it to remove from loop
         ElseIf ( its(izb) == 1 ) Then
-          If ( idiag >= 0 ) Write(lun_diag,"(a,2(i5,a))") 'KStep ',kstep,' Zone ',izone,' Inter!!!'
           its(izb) = 2
+          lzsolve(izb) = .false.
+          lzoutput(izb) = .false.
         EndIf
       EndDo
 
-      lzoutput = ( lzsolve .and. its <= 0 )
-      Call ts_output(kstep,delta_en,edot,mask_in = lzoutput)
+      !$acc update wait(tid) &
+      !$acc host(lzoutput,lzsolve)
+      Call ts_output(kstep,en0,enold,mask_in = lzoutput)
 
       ! Test if all zones have stopped
-      lzsolve = ( its == 0 )
       If ( .not. any( lzsolve ) ) Exit
     EndDo
+
+    !$acc exit data wait(tid) &
+    !$acc copyout(its,mykstep) &
+    !$acc delete(lzsolve,lzoutput)
 
     ! Test that the stop time is reached
     Do izb = zb_lo, zb_hi
       If ( lzactive(izb) ) Then
+        !$acc update wait(tid) &
+        !$acc host(t(izb),tdel(izb))
         If ( t(izb) < tstop(izb) .or. its(izb) > 0 ) Then
           izone = izb + szbatch - zb_lo
           Write(lun_stdout,"(a,i5,a,3(es12.4,a))") &
@@ -181,6 +195,8 @@ Contains
         EndIf
       EndIf
     EndDo
+
+    kstep = max(1, maxval(mykstep))
 
     stop_timer = xnet_wtime()
     timer_xnet = timer_xnet + stop_timer
