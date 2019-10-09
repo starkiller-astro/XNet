4cf26e7973e13765fd94ac81665767d341f03891
Austin Harris <jaharris87@users.noreply.github.com>
2019-10-09 10:37:48 -0400

    Add additional y_moment routine to operate on all zones

--- source/xnet_abundances.f90
+++ source/xnet_abundances.f90
@@ -51,4 +51,82 @@ Contains
     Return
   End Subroutine y_moment
 
+  Subroutine y_moment2(y,ye,ytot,abar,zbar,z2bar,zibar,mask_in)
+    !-----------------------------------------------------------------------------------------------
+    ! This routine calculates moments of the abundance distribution for the EOS.
+    !-----------------------------------------------------------------------------------------------
+    Use nuclear_data, Only: ny, aa, zz, zz2, zzi
+    Use xnet_controls, Only: idiag, lun_diag, zb_lo, zb_hi, lzactive, tid
+    Use xnet_types, Only: dp
+    Implicit None
+
+    ! Input variables
+    Real(dp), Intent(in) :: y(ny,zb_lo:zb_hi)
+
+    ! Output variables
+    Real(dp), Intent(out) :: ye(zb_lo:zb_hi), ytot(zb_lo:zb_hi), abar(zb_lo:zb_hi)
+    Real(dp), Intent(out) :: zbar(zb_lo:zb_hi), z2bar(zb_lo:zb_hi), zibar(zb_lo:zb_hi)
+
+    ! Optional variables
+    Logical, Optional, Target, Intent(in) :: mask_in(zb_lo:zb_hi)
+
+    ! Local variables
+    Integer :: izb, k
+    Real(dp) :: ntot, atot, ztot, z2tot, zitot
+    Logical, Pointer :: mask(:)
+
+    If ( present(mask_in) ) Then
+      mask(zb_lo:) => mask_in
+    Else
+      mask(zb_lo:) => lzactive(zb_lo:zb_hi)
+    EndIf
+    If ( .not. any(mask) ) Return
+
+    !$acc enter data async(tid) &
+    !$acc copyin(mask)
+
+    ! Calculate abundance moments
+    !$acc kernels loop gang async(tid) &
+    !$acc present(mask,y,aa,zz,zz2,zzi,ytot,ye,abar,zbar,z2bar,zibar) &
+    !$acc private(ntot,atot,ztot,z2tot,zitot)
+    Do izb = zb_lo, zb_hi
+      If ( mask(izb) ) Then
+        ntot  = 0.0
+        atot  = 0.0
+        ztot  = 0.0
+        z2tot = 0.0
+        zitot = 0.0
+        !$acc loop vector &
+        !$acc reduction(+:ntot,atot,ztot,z2tot,zitot)
+        Do k = 1, ny
+          ntot  = ntot  + y(k,izb)
+          atot  = atot  + y(k,izb) * aa(k)
+          ztot  = ztot  + y(k,izb) * zz(k)
+          z2tot = z2tot + y(k,izb) * zz2(k)
+          zitot = zitot + y(k,izb) * zzi(k)
+        EndDo
+        ytot(izb)  = ntot
+        ye(izb)    = ztot  / atot
+        abar(izb)  = atot  / ntot
+        zbar(izb)  = ztot  / ntot
+        z2bar(izb) = z2tot / ntot
+        zibar(izb) = zitot / ntot
+      EndIf
+    EndDo
+    If ( idiag >= 3 ) Then
+      !$acc update wait(tid) &
+      !$acc host(ytot,abar,zbar,z2bar,zibar,ye)
+      Do izb = zb_lo, zb_hi
+        If ( mask(izb) ) Then
+          Write(lun_diag,"(a4,6es23.15)") 'YMom',ytot,abar,zbar,z2bar,zibar,ye
+        EndIf
+      EndDo
+    EndIf
+
+    !$acc exit data async(tid) &
+    !$acc delete(mask)
+
+    Return
+  End Subroutine y_moment2
+
 End Module xnet_abundances
