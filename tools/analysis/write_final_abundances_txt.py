16df046c9d58d0f66911703e167fda23bc19b13a
Andre Sieverding <andre.siever@gmail.com>
2021-06-15 12:38:34 -0400

    output format improvements in write_final_abundances_txt

--- tools/analysis/write_final_abundances_txt.py
+++ tools/analysis/write_final_abundances_txt.py
@@ -1,9 +1,12 @@
-def write_final_abundances(directory_files, file_name = "test.txt", appending = False):
+def write_final_abundances(directory_files, file_name = "test.txt", appending = False, print_positions=False, last=False):
     
     '''
         Inputs: directory_files: the full String pathway to the desired files, suggested ending is /ts_final_*
                 file_name = String that names the file created or designates which file to append data to
                 appending: boolean where False writes a new file and True adds new data onto file_name, defaults to False
+                print_positions: boolean where True prints file positions to screen (mostly for debug), defaults to False
+                last : boolean where True skips to the end of the file to just read the final abundances, defaults to False
+                print_positions and last are passed to read_ts_file
         Output: a new file, "file_name", or an expanded file, with final abundance data for each species
         '''
     import numpy as np
@@ -26,14 +29,13 @@ def write_final_abundances(directory_files, file_name = "test.txt", appending = 
     number_of_files = len(files)    
 
     non_ejecta = []
-                   
     for datafile in files:
 
        	print (datafile)
         print ("%s" % counter)
 
 	 #Read data file, use variable names.
-        zz, aa, xmf, time, temperature, density, timestep, edot, flx_end, flx = rtf.read_ts_file(datafile, last = True)
+        zz, aa, xmf, time, temperature, density, timestep, edot, flx_end, flx = rtf.read_ts_file(datafile,last=last,print_positions=print_positions)
         nuc_name = rtf.build_isotope_symbol(zz, aa)
 
         #Set certain parameters based on the size of the data.
@@ -43,10 +45,14 @@ def write_final_abundances(directory_files, file_name = "test.txt", appending = 
         
         #Write header with species names only as a first line.
         if counter == 1:
-            f.writelines("%s " % item for item in nuc_name)
-            f.writelines("\n")
+            # set size of padding based on first filename
+            padding = len(datafile)+5
+            line = " ".ljust(padding)
+            for item in nuc_name:
+                line = line + item.ljust(14)
+            f.write( line + "\n" )
 
-        if timesteps_total < 10:
+        if (not last) and (timesteps_total < 10):
             non_ejecta.append(datafile)
             counter += 1
         
@@ -58,9 +64,10 @@ def write_final_abundances(directory_files, file_name = "test.txt", appending = 
                 xmf_list.append(foo)
 
             #Write datafile name, each final abundance from xmf_list, and go to a new line at the end of the file.
-            f.writelines ("%s " % datafile)
-            f.writelines ("%s " % item for item in xmf_list)
-            f.writelines ("\n")
+            line = datafile.ljust(padding)
+            for item in xmf_list:
+                line = line + str("%.5e" % item).ljust(14)
+            f.writelines (line+"\n")
 
             #Makes the header print only once.
             counter += 1
