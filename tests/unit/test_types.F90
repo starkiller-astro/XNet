416e0013fa6247886ab920dd31289cd77ab4fabe
Austin Harris <jaharris87@users.noreply.github.com>
2026-08-03 16:09:45 -0400

    Define i4 as int32 (#20)
    Co-authored-by: Harris, Austin <harrisja@ornl.gov>

--- tests/unit/test_types.F90
+++ tests/unit/test_types.F90
@@ -1,7 +1,7 @@
 Module test_types
-  Use, Intrinsic :: iso_fortran_env, Only: int64, real32, real64
+  Use, Intrinsic :: iso_fortran_env, Only: int32, int64, real32, real64
   Use testdrive, Only: check, error_type, new_unittest, unittest_type
-  Use xnet_types, Only: dp, i8, sp
+  Use xnet_types, Only: dp, i4, i8, sp
   Implicit None
   Private
 
@@ -25,6 +25,8 @@ Contains
     If ( Allocated(error) ) Return
     Call check(error, sp == real32, "sp must equal iso_fortran_env real32")
     If ( Allocated(error) ) Return
+    Call check(error, i4 == int32, "i4 must equal iso_fortran_env int32")
+    If ( Allocated(error) ) Return
     Call check(error, i8 == int64, "i8 must equal iso_fortran_env int64")
   End Subroutine test_kind_values
 
@@ -34,6 +36,8 @@ Contains
     Call check(error, Storage_Size(0.0_dp) == 64, "real(dp) must occupy 64 bits")
     If ( Allocated(error) ) Return
     Call check(error, Storage_Size(0.0_sp) == 32, "real(sp) must occupy 32 bits")
+    If ( Allocated(error) ) Return
+    Call check(error, Storage_Size(0_i4) == 32, "integer(i4) must occupy 32 bits")
     If ( Allocated(error) ) Return
     Call check(error, Storage_Size(0_i8) == 64, "integer(i8) must occupy 64 bits")
   End Subroutine test_storage_sizes
